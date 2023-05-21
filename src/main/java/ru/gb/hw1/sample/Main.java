eca86b93069ddc54803b611f096ffe356c27703e
Leo <leonidamvrosov@yandex.ru>
2023-05-21 21:47:38 +0500

    javac,javadoc added

--- src/main/java/ru/gb/hw1/sample/Main.java
+++ src/main/java/ru/gb/hw1/sample/Main.java
@@ -4,6 +4,14 @@ import ru.gb.hw1.regular.Decorator;
 import ru.gb.hw1.regular.OtherClass;
 
 public class Main {
+
+    /**
+     * javac -sourcepath ./java -d out java/ru/gb/hw1/sample/Main.java
+     * <p>
+     * java -classpath ./out ru.gb.hw1.sample.Main
+     * <p>
+     * javadoc -encoding utf8 -d docs -sourcepath ./java -cp ./out -subpackages ru
+     */
     public static void main(String[] args) {
         int result = OtherClass.add(23, 45);
         System.out.println(Decorator.decorate(result));
